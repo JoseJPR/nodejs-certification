f5c94a1487297ef5697185fd39cb1e0042564638
JoseJPR <me@josejpr.com>
2019-11-24 12:36:03 +0100

    Remove innecesary files

--- src/child-processes/sub.mjs
+++
@@ -1,6 +0,0 @@
-process.on('message', (m) => {
-  console.log('CHILD got message:', m);
-});
-
-// Causes the parent to print: PARENT got message: { foo: 'bar', baz: null }
-process.send({ foo: 'bar', baz: 'child' });
